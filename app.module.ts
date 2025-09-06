1790f08c32d23e02af8c3dbc5694aaa2c42ff5fb
haiminh122003 <haiminh122003@gmail.com>
2025-09-06 17:04:46 +0700

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,17 @@
+// src/app.module.ts
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TasksModule } from './tasks/tasks.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // tự động tạo bảng
+    }),
+    TasksModule,
+  ],
+})
+export class AppModule {}
